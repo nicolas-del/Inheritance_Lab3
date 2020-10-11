38a121a25415aa2400ee33eccfd450dd7976af1c
Nicolas Espitalier <nespitalier013@gmail.com>
2020-10-11 17:25:25 -0400

    Add project files.

---
+++ Inheritance_Lab3/IAccount.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance_Lab3
+{
+    interface IAccount
+    {
+        void MakeWithdrawl(double amount);
+        void MakeDeposit(double amount);
+        void CalculateInterest();
+        string CloseAndReport();
+    }
+}
