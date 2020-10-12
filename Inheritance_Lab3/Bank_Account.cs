3e1cf424bdd037cf489b4290d3801f8cd47be2db
Nicolas Espitalier <nespitalier013@gmail.com>
2020-10-11 21:09:51 -0400

    Partial completion of the Bank_Account class

--- Inheritance_Lab3/Bank_Account.cs
+++ Inheritance_Lab3/Bank_Account.cs
@@ -6,45 +6,69 @@ using System.Threading.Tasks;
 
 namespace Inheritance_Lab3
 {
+    protected enum status 
+    { 
+        active, inactive
+    }
+
     class Bank_Account : IAccount
     {
 
-        private double starting_balance;
-        private double current_balance;
-        private int numberOfDeposits = 0;
+        protected double starting_balance;  
+        protected double current_balance;  
+        protected double totalOfDepositsThisMonth; 
+        protected double totalOfWithdrawalThisMonth; 
+        protected double annualInterestRate; 
+        protected double monthServiceCharge; 
+        protected int numberOfDeposits = 0;
+        protected int numberOfWithdrawal = 0;
 
-        public double Starting_Balance { get { return starting_balance; } set { starting_balance = value; } }
-        public double Current_Balance { get { return current_balance; } set { current_balance = value; } }
+        public double Starting_Balance { get { return starting_balance; } }
+        public double Current_Balance { get { return current_balance; } }
 
         public Bank_Account() { }
 
-        public Bank_Account(double sb, double cb) 
+        public Bank_Account(double sb, double annualRate) 
         {
             starting_balance = sb;
-            current_balance = cb;
+            annualInterestRate = annualRate;
         }
-
 
 
         public void CalculateInterest()
         {
-            
+            double monthlyInterest, monthlyInterestRate;
+
+            monthlyInterestRate = annualInterestRate / 12;
+            monthlyInterest = current_balance * monthlyInterestRate;
+            current_balance += monthlyInterest;
         }
 
-        public string CloseAndReport()
+        public virtual string CloseAndReport()
         {
-            
+            double result; 
+            result = monthServiceCharge - current_balance;
+            CalculateInterest();
+            numberOfWithdrawal = 0; 
+            numberOfDeposits = 0;  
+            monthServiceCharge = 0; 
+
+            return "The balance at the beginning of the month was of " + starting_balance + ". Now it is of " + current_balance + " which minus the monthly service charge equals " + result + 
+                   ".\nAnnual interest rate: " + annualInterestRate + 
+                   "\nTotal deposits this month: " + totalOfDepositsThisMonth + 
+                   "\nTotal of withdrawal this month: " + totalOfWithdrawalThisMonth;
         }
 
-        public void MakeDeposit(double amount)
+        public virtual void MakeDeposit(double amount)
         {
-            current_balance = starting_balance + amount;
+            starting_balance += amount;
             numberOfDeposits++;
         }
 
-        public void MakeWithdrawl(double amount)
+        public virtual void MakeWithdrawl(double amount)
         {
-            
+            starting_balance -= amount;
+            numberOfWithdrawal++;
         }
     }
 }
