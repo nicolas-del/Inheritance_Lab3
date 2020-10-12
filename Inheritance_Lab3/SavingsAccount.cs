55eb4bbb2a7442f496dd09536200b1de72af77dc
Ajay Jose <ajaypjose@hotmail.com>
2020-10-11 18:47:11 -0700

    Created new class SavingsAccount.

---
+++ Inheritance_Lab3/SavingsAccount.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance_Lab3
+{
+    class SavingsAccount : Bank_Account
+    {
+        public virtual void MakeWithDraw(protected enum status)
+        {
+
+    }
+}
